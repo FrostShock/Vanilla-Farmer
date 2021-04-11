2f5d8fc12dbf62b27fbe3dccd91e1452a9c2bb5e
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-04-11 13:42:57 +0200

    Add files via upload

--- Scripts/0-copy-chat-settings.bat
+++ Scripts/0-copy-chat-settings.bat
@@ -1,11 +1,11 @@
-ls -l | grep "^d" | grep -v SavedVariables | cut -c 44- > 01-realm.txt
+ls -l | grep "^d" | grep -v SavedVariables | sed -e "s/.* //" > 01-realm.txt
 set /p server=<01-realm.txt
 
 echo %server%
 
 ls -l %server% | grep "^d" | cut -c 40- > 01-toons.txt
 
-cat 01-toons.txt | sed -e "s/\(.*\)/copy chat-cache.txt %server%\\\1/" > 02-copy.bat
+cat 01-toons.txt | sed -e "s/\(.*\)/copy chat-cache.txt %server%\\\1/" > 02-copy-chat-settings.bat
 echo pause>>02-copy-chat-settings.bat
 
-pause
+call 02-copy-chat-settings.bat
