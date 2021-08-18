76cbb6fee9fcf2e07c0511688abbb3364943d1f0
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-08-19 00:06:08 +0200

    Add files via upload

---
+++ Settings/Omnibus.lua
@@ -0,0 +1,18 @@
+
+OmnibusDB = {
+	["account"] = {
+		["book"] = {
+			[1] = {
+				[2] = "MapCoords   -   /mc player   -   /mc wc\nDecursive   -   /dcrshow\nAntagonist   -   /antagonist lock\nChronometer   -   /chronometer anchor\n\nAUX:\n   /aux post duration 24\n   /aux tooltip value\nGatherer:\n   /gather mining on\n   /gather herbs off\n   /gather treasure toggle\n\nMinimapButtonBag   -   use Ctrl + Right if you want to reattach any button to the minimap\n\nBestBuff   -    type “/bestbuff” [Enter], then check “Enable”.\n\nCrafty   -   If you right-click on an item, it will apply a filter so you can only see that item in the trade window. In order to get rid of the filter, right-click again on that item.\n\n\n",
+			},
+		},
+	},
+	["profiles"] = {
+		["Default"] = {
+			["minimapPosition"] = 209.9529076845996,
+			["detachedTooltip"] = {
+				["fontSizePercent"] = 1,
+			},
+		},
+	},
+}
