76a3762772cb77b75c47249b3501223e2529fb35
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-15 06:39:13 +0200

    Update 0-Gatherer-Chests.bat

--- Scripts/0-Gatherer-Chests.bat
+++ Scripts/0-Gatherer-Chests.bat
@@ -1,4 +1,4 @@
 rem This script removes from Gatherer database all the items that are not treasure chests
 rem gawk -f gdelete.awk Gatherer.lua > Gatherer1.lua
-gawk -f 1-chests.awk Gatherer.lua > Gatherer-chests.lua
+gawk -f 1-Gatherer-Chests.awk Gatherer.lua > Gatherer-chests.lua
 pause
