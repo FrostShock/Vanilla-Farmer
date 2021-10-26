f1b57817db4845fba1cccda407885238528f900f
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-26 15:28:11 +0200

    Add files via upload

---
+++ Settings/ShaguTweaks.lua
@@ -0,0 +1,37 @@
+
+ShaguTweaks_config = {
+	["Hide Gryphons"] = 1,
+	["MiniMap Clock"] = 1,
+	["Auto Dismount"] = 1,
+	["WorldMap Window"] = 0,
+	["Unit Frame Health Colors"] = 0,
+	["Item Rarity Borders"] = 0,
+	["MiniMap Tweaks"] = 0,
+	["Nameplate Scale"] = 0,
+	["Hide Errors"] = 0,
+	["Movable Unit Frames"] = 0,
+	["Debuff Timer"] = 0,
+	["MiniMap Square"] = 0,
+	["Vendor Values"] = 1,
+	["Nameplate Class Colors"] = 1,
+	["Equip Compare"] = 1,
+	["Enemy Castbars"] = 1,
+	["WorldMap Coordinates"] = 1,
+	["Blue Shaman Class Colors"] = 1,
+	["Social Colors"] = 1,
+	["Unit Frame Class Colors"] = 1,
+	["Real Health Numbers"] = 1,
+	["Chat Tweaks"] = 1,
+	["Darkened UI"] = 0,
+	["Sell Junk"] = 0,
+	["WorldMap Class Colors"] = 1,
+	["Auto Stance"] = 1,
+	["Chat Hyperlinks"] = 1,
+	["Tooltip Details"] = 1,
+}
+ShaguTweaks_cache = {
+	["players"] = {
+	},
+	["libhealth"] = {
+	},
+}
